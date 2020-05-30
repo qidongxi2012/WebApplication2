c1c73e151a83824f8a557b29208fa3a0452910d2
qidongxi2012 <478092336@qq.com>
2020-05-31 07:00:17 +0800

    master222222

--- WebApplication2/Controllers/WgyController.cs
+++ WebApplication2/Controllers/WgyController.cs
@@ -16,7 +16,7 @@ namespace WebApplication2.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","master","master1"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","master","master1","master2222"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
